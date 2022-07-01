80119327142a13b755fb018c613400a03062725d
WeiChunKao <34979078+WeiChunKao@users.noreply.github.com>
2022-07-01 10:28:04 +0800

    加入專案檔案。

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+     class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            Console.WriteLine("Press any key..");
+            Console.ReadLine();
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine($"Greetings:{reply.Message}");
+            Console.ReadKey();
+        }
+    }
+}
